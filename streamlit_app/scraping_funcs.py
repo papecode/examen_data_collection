e086fcaf9b4ef6858e3b07bfcb4d0975b8466939
papecode <papesaliouwade500@gmail.com>
2025-07-16 14:23:32 +0000

    final version

--- streamlit_app/scraping_funcs.py
+++ streamlit_app/scraping_funcs.py
@@ -1,22 +1,18 @@
 import pandas as pd
 import time
 import re
+import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-
 
 def clean_text(text):
     return re.sub(r'\s+', ' ', text.strip().replace('\u202f', '')).replace('\n', '').replace('Par ', '')
-
 
 def scrape_voitures(pages=1):
     data = []
     for page in range(1, pages + 1):
         url = f"https://dakar-auto.com/senegal/voitures-4?page={page}"
-        driver.get(url)
-        soup = BeautifulSoup(driver.page_source, "html.parser")
+        res = requests.get(url)
+        soup = BeautifulSoup(res.text, "html.parser")
         annonces = soup.select(".listings-cards__list-item")
 
         for annonce in annonces:
@@ -47,13 +43,12 @@ def scrape_voitures(pages=1):
         time.sleep(1)
     return pd.DataFrame(data, columns=["marque", "annee", "prix", "adresse", "kilometrage", "boite_vitesse", "carburant", "proprietaire"])
 
-
 def scrape_motos(pages=1):
     data = []
     for page in range(1, pages + 1):
         url = f"https://dakar-auto.com/senegal/motos-and-scooters-3?page={page}"
-        driver.get(url)
-        soup = BeautifulSoup(driver.page_source, "html.parser")
+        res = requests.get(url)
+        soup = BeautifulSoup(res.text, "html.parser")
         annonces = soup.select(".listings-cards__list-item")
 
         for annonce in annonces:
@@ -80,13 +75,12 @@ def scrape_motos(pages=1):
         time.sleep(1)
     return pd.DataFrame(data, columns=["marque", "annee", "prix", "adresse", "kilometrage", "proprietaire"])
 
-
 def scrape_locations(pages=1):
     data = []
     for page in range(1, pages + 1):
         url = f"https://dakar-auto.com/senegal/location-de-voitures-19?page={page}"
-        driver.get(url)
-        soup = BeautifulSoup(driver.page_source, "html.parser")
+        res = requests.get(url)
+        soup = BeautifulSoup(res.text, "html.parser")
         annonces = soup.select(".listings-cards__list-item")
 
         for annonce in annonces:
@@ -106,9 +100,3 @@ def scrape_locations(pages=1):
                 continue
         time.sleep(1)
     return pd.DataFrame(data, columns=["marque", "annee", "prix", "adresse", "proprietaire"])
-
-
-# Setup Selenium driver
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-driver = webdriver.Chrome(options=chrome_options)
